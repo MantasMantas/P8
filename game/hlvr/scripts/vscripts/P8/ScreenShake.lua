f0130a05d0563f03c1b0abe624647a7123945282
unknown <xmantissx@gmail.com>
2021-05-18 14:04:59 +0200

    tuesday 18th

---
+++ game/hlvr/scripts/vscripts/P8/ScreenShake.lua
@@ -0,0 +1,9 @@
+local center = Vector(1,1,1)
+local amplitude = 60
+local frequency = 120
+local duration = 5
+local radius = 60
+
+function ScreenShake()
+    ScreenShake(center, amplitude, frequency, duration, radius, 0, true)
+end
--- game/hlvr/scripts/vscripts/P8/ShiftScript.lua
+++ game/hlvr/scripts/vscripts/P8/ShiftScript.lua
@@ -26,11 +26,26 @@ end
 
 function ShiftCorridor()
     --corridor = Entities:FindByName(nil, "Corridor1")
-    corridor = EntIndexToHScript(495)
-    doorFrame = Entities:FindByName(nil, "DoorFrame1_1")
-    door = Entities:FindByName(nil, "Door1_1")
+    --corridor = EntIndexToHScript(495)
+    --doorFrame = Entities:FindByName(nil, "DoorFrame1_1")
+    --door = Entities:FindByName(nil, "Door1_1")
 
-    corridor:SetOrigin(Vector(-17.4503,116.265,64.0148))
-    doorFrame:SetOrigin(Vector(-36.6565,29.3683,0))
-    door:SetOrigin(Vector(-18.5504,28.7339,1))
+    --corridor:SetOrigin(Vector(-17.4503,116.265,64.0148))
+    --doorFrame:SetOrigin(Vector(-36.6565,29.3683,0))
+    --door:SetOrigin(Vector(-18.5504,28.7339,1))
+end
+
+function FirstShift()
+        local group1 = Entities:FindByName(nil, "shift_group_1")
+        local group1pos = Vector(-15.7291, 67.2933, 64.1559)
+
+        local group2 = Entities:FindByName(nil, "shift_group_2")
+        local group2pos = Vector(-32.0325, 29.5918, 64)
+
+        local door = Entities:FindByName(nil, "Door1_1")
+        local doorpos = Vector(-13.9264, 28.7339, 1)
+
+        group1:SetOrigin(group1pos)
+        group2:SetOrigin(group2pos)
+        door:SetOrigin(doorpos)
 end
